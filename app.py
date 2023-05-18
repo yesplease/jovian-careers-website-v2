932c0daadced37c473bc5a7ca3e4e312582f918a
yesplease <bradley.heinz@gmail.com>
2023-05-18 21:55:00 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
+import os
+
+my_secret = os.environ['DB_CONNECTION_STRING']
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_jovian():
   jobs = load_jobs_from_db()
-  return render_template('home.html', 
-                        jobs=jobs, 
-                        company_name='Jovian')
+  return render_template('home.html', jobs=jobs)
+
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  print('this is ID in app.route /job/<id>')
+  print(id)
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html',
+                        job=job)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,16 +1,13 @@
-import sqlalchemy
-from sqlalchemy import create_engine, text
 import os
+from sqlalchemy import create_engine, text
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args = {
-    'ssl': {
-      'ssl_ca': "/etc/ssl/cert.pem"
-    }
-  })
+engine = create_engine(db_connection_string,
+                       connect_args={'ssl': {
+                         'ssl_ca': "/etc/ssl/cert.pem"
+                       }})
+
 
 def load_jobs_from_db():
   with engine.connect() as conn:
@@ -19,3 +16,16 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(row._asdict())
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    print("this is loadjobfromdb id")
+    print(id)
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = {id}"))
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
