649227a4102c7234e1acdcae485af322a3a281b0
yesplease <bradley.heinz@gmail.com>
2023-05-18 23:37:19 +0000

    Added ability to save application to db

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
 
 my_secret = os.environ['DB_CONNECTION_STRING']
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_df
 
 app = Flask(__name__)
 
@@ -27,6 +27,17 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html',
+                        job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  job = load_job_from_db(id)
+  data = request.form
+  #store in db
+  add_application_to_df(id, data)
+  
+  return render_template('application_submitted.html',
+                        application=data,
                         job=job)
 
 
