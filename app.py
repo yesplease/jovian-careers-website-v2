31ed4d3f60a96ecdf9b034e8a52d9f49090c42b1
yesplease <bradley.heinz@gmail.com>
2023-05-19 20:40:39 +0000

    add captcha

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
+from flask_xcaptcha import XCaptcha
 
 my_secret = os.environ['DB_CONNECTION_STRING']
+captcha_secret = os.environ['CAPTCHA_SECRET']
+chaptcha_site_key = 'da3aea6f-769c-4cdd-ac6d-6d7764548249'
 from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db, add_application_to_df
 
 app = Flask(__name__)
-
+app.config['XCAPTCHA_SITE_KEY'] = chaptcha_site_key
+app.config['XCAPTCHA_SECRET_KEY'] = captcha_secret
+app.config['XCAPTCHA_VERIFY_URL'] = "https://hcaptcha.com/siteverify"
+app.config['XCAPTCHA_API_URL'] = "https://hcaptcha.com/1/api.js"
+app.config['XCAPTCHA_DIV_CLASS'] = "h-captcha"
+xcaptcha = XCaptcha(app=app)
 
 @app.route("/")
 def hello_jovian():
@@ -33,12 +41,26 @@ def show_job(id):
 def apply_to_job(id):
   job = load_job_from_db(id)
   data = request.form
+
+  #server-side validation of captcha
+  if xcaptcha.verify():
+    return render_template('application_submitted.html',
+                        application=data,
+                        job=job)
+  else:
+    return render_template('captcha_error.html',
+                        application=data)
+  
   #store in db
   add_application_to_df(id, data)
   
-  return render_template('application_submitted.html',
-                        application=data,
-                        job=job)
+
+
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
 
 
 if __name__ == '__main__':
